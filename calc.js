6304285f8ac42b4013c5b2d53f774754b1273af7
Olivier Perron <1816498@collegemultihexa.ca>
2018-11-08 11:41:34 -0500

    bonsoir

--- calc.js
+++ calc.js
@@ -1,6 +1,35 @@
-var packs = $.getJSON("packs.json").done(function(){console.log("packs.json loading success")})
+var packsdata
 
-$(function() {
-    // populate pack list
+$(document).ready(function () {
+    $.when($.getJSON("packs.json")).done(function (data) {
+        packsdata = data;
+        console.log("Loaded packs.json");
+        initLists();
 
-})
+    });
+});
+
+function initLists() {
+    // Populate region list (only done once)
+    for (i = 0; i < packsdata.length; i++) {
+        var option = $("<option></option>").text(packsdata[i].region);
+        option.attr("value", i);
+        $("#region").append(option);
+    };
+
+    // Populate packs
+    var regionID = $("#region").find(":selected").attr("value");
+    for (i = 0; i < packsdata[regionID].packs.length; i++) {
+        var option = $("<option></option>").text(packsdata[regionID].packs[i].name);
+        option.attr("value", i);
+        $("#pack").append(option);
+    };
+
+    //Populate outlets
+    var packID = $("#pack").find(":selected").attr("value");
+    for (i = 0; i < packsdata[regionID].packs[packID].prices.length; i++) {
+        var option = $("<option></option>").text(packsdata[regionID].packs[packID].prices[i].outlet);
+        option.attr("value", i);
+        $("#outlet").append(option);
+    };
+}
